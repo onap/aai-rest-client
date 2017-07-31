29c93067b1b2ae60d6db54c1d604854f771a91ae
ARUL NAMBI <arul.nambi@amdocs.com>
2017-07-31 13:40:09 -0400

    Changing the license and trademark
    Issue-ID: AAI-88
    Change-Id: I3a5641a294c4072734aca565369cf759a9030cf9
    Signed-off-by: ARUL NAMBI <arul.nambi@amdocs.com>

--- src/main/java/org/openecomp/restclient/client/Headers.java
+++ src/main/java/org/openecomp/restclient/client/Headers.java
@@ -1,16 +1,15 @@
 /**
  * ============LICENSE_START=======================================================
- * RestClient
+ * org.onap.aai
  * ================================================================================
- * Copyright © 2017 AT&T Intellectual Property.
+ * Copyright © 2017 AT&T Intellectual Property. All rights reserved.
  * Copyright © 2017 Amdocs
- * All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,8 +18,7 @@
  * limitations under the License.
  * ============LICENSE_END=========================================================
  *
- * ECOMP and OpenECOMP are trademarks
- * and service marks of AT&T Intellectual Property.
+ * ECOMP is a trademark and service mark of AT&T Intellectual Property.
  */
 package org.openecomp.restclient.client;
 
